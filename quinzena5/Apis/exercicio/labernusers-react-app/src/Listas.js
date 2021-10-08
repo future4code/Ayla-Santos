eb7e9e9701c498fe570bc5d5644b377536adb8b1
Ayla1524 <aylasilva1524@outlook.com>
2021-10-07 23:20:47 -0300

    adicionando exercicio

---
+++ quinzena5/Apis/exercicio/labernusers-react-app/src/Listas.js
@@ -0,0 +1,16 @@
+import React from react 
+    
+class App extends React.components{
+     render(){
+         return(
+             <div className="App">
+              <h1>LABENUSERS</h1>
+              <h2>Lista de Usuarios</h2>
+             </div>
+         )
+     }
+}
+
+
+
+
