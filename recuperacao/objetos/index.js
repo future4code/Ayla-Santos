957034f2ae9baaa200b4653b46ea9a3313c813d8
Ayla1524 <aylasilva1524@outlook.com>
2021-10-09 00:57:38 -0300

    adicionando objetos

---
+++ recuperacao/objetos/index.js
@@ -0,0 +1,83 @@
+// Exercício de Escrita 
+
+// Numero 1 
+// const pessoa= {
+// nome:"Ayla",
+// apelido:["Ay", "lala" ,"Aya"],
+// }
+
+// console.log(`Oi meu nome é ${pessoa.nome} mas, pode me chamar de ${pessoa.apelido}`)
+
+// letra A 
+  
+// const novaPessoa ={
+//     ...pessoa,
+//     apelido:["pequena" , "pretinha"]
+// }
+
+// console.log(novaPessoa)
+
+
+// numero 2
+//  letra A 
+
+// const pessoa1 ={
+//     nome:"Michel",
+//     idade:30,
+//     profissão:"economista"
+// }
+
+// console.log(pessoa1)
+
+// const pessoa2 ={
+//     nome:"iaura",
+//     idade:61,
+//     profissão:"revendedora"
+// }  
+// console.log(pessoa2)
+     
+// letra B
+// const pessoa ={
+    //       nome:"ielida",
+    //       idade:24,
+    //       profissão:"Tecnica-enfermagem",
+
+        
+    //   }  
+    
+    // console.log([pessoa.nome, pessoa.nome.length, pessoa.idade, pessoa.profissão, pessoa.profissão.length])
+  
+
+// Numero 4 
+
+function funcao1(){
+    const carrinho=[]
+}
+  const fruta1={
+      nome:"Abacaxi",
+      disponibilidade:"true"
+  }
+console.log(fruta1)
+
+const fruta2={
+    nome:"Pêra",
+    disponibilidade:"true",
+}
+console.log(fruta2)
+
+const fruta3={
+    nome:"Morango",
+    disponibilidade:"true",
+}
+console.log(fruta3)
+ 
+function imprimirCarrinho (fruta1){
+const carrinho=[]
+    carrinho.push([fruta1])
+     carrinho.push([fruta2])
+     carrinho.push([fruta3])
+console.log(carrinho)
+}
+  imprimirCarrinho(fruta1)
+   
+
