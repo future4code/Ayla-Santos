955314dda460b926296fe1d3c49eeca292028fff
Ayla1524 <aylasilva1524@outlook.com>
2021-10-13 21:15:58 -0300

    adicionando laços

---
+++ revisao/laços/LAÇOS/index.js
@@ -0,0 +1,57 @@
+// exercicio de escrita 
+
+// Numero 1
+
+// const bichinhoUsuario = Number(prompt("Quantos animais de estimação você tem?"))
+
+
+// for (let i =0 ; i < 0; i++ ){
+//  const elemento = bichinhoUsuario[i]
+//   console.log(elemento)
+// }
+
+// if (bichinhoUsuario === 0){
+//     console.log("Que pena! você não pode adotar um pet")
+// } else if (bichinhoUsuario > 0){
+//     console.log (prompt("Digite o nome deles"))
+// }
+
+
+// const bichosDoUsuario = []
+ 
+// bichosDoUsuario.push(bichinhoUsuario)
+// console.log(bichosDoUsuario)
+
+
+// numero 2 
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+let i = 0
+
+// letra A
+// while(i< array){
+//     console.log(array)
+
+//     i++
+// }
+
+// console.log(array)
+
+// letra B 
+
+// let arrayDividido= -infinity
+
+// while(i < arrayDividido){
+//     if(arrayDividido[i] /10){
+
+//     }
+//     i++
+// }
+
+// console.log(arrayDividido)
+
+
+// letra c 
+//  let arrayPares = array
+
+//  for(){}
